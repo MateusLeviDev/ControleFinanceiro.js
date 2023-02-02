2a0bf8c858482bf7647228fd4d10733dbcdc22c6
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-02 18:26:30 -0300

    [Remove Item]

--- script.js
+++ script.js
@@ -9,12 +9,10 @@ const inputTransactionAmount = document.querySelector('#amount')
 
 // declaramos um array de objetos para simular algumas transações 
 
-const dummyTrasactions = [
-    { id: 1, name: 'Bolo de brigadeiro', amount: -20 }, 
-    { id: 2, name: 'Salário', amount: 300 }, 
-    { id: 3, name: 'Torta', amount: -10 }, 
-    { id: 4, name: 'Violão', amount: 150 }, 
-]
+const localStorageTransactions = JSON.parse(localStorage
+    .getItem('transactions'))
+let transactions = localStorage
+    .getItem('transactions') !== null ? localStorageTransactions : []
 
 //criaremos uma função que adcionará as transações no DOM
 
@@ -22,29 +20,38 @@ const dummyTrasactions = [
 
 para que essa url seja preenchida  */
 
-const addTransactionIntoDOM = trasaction => {
+const removeTransaction = ID => {
+    //remover do DOM a transação que foi clicada. Faremos com que esse array dummy receba as transações que ele ja tem, excente a clicada
+    transactions = transactions
+    .filter(transaction => 
+        transaction.id !== ID)
+    init()
+}
+
+const addTransactionIntoDOM = transaction => {
     // dentro dessa função iremos gerar uma li com os dados 
-    const operator = trasaction.amount < 0 ? '-' : '+';
-    const CSSClass = trasaction.amount < 0 ? 'minus' : 'plus'
-    const amountWithoutOperator = Math.abs(trasaction.amount)
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
     const li = document.createElement('li') //met que criamos novo element html
 
     li.classList.add(CSSClass)
     li.innerHTML = `
-    ${trasaction.name} <span>${operator} R$ ${amountWithoutOperator}
-    </span><button class="delete-btn">x</button>
+    ${transaction.name} 
+    <span>${operator} R$ ${amountWithoutOperator}</span>
+    <button class="delete-btn" onClick="removeTransaction(${transaction.id})">
+    x
+    </button>
     `
     transactionsUl.prepend(li)
 
 } // ctrl + k + f identação -----  shift alt mais seta
 
 const updateBalanceValues = () => {
-    const transactionsAmount = dummyTrasactions
-        .map
-        (trasaction => trasaction.amount)
+    const transactionsAmount = transactions
+        .map(transaction => transactions.amount)
     const total = transactionsAmount
-        .reduce(
-        (accumulator, transaction) => accumulator + transaction, 0)
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
         .toFixed(2) 
     // utilizaremos um método filter 
     const income = transactionsAmount
@@ -63,7 +70,8 @@ const updateBalanceValues = () => {
 }
 
 const init = () => {
-    dummyTrasactions.forEach(addTransactionIntoDOM)
+    transactionsUl.innerHTML = ''
+    transactions.forEach(addTransactionIntoDOM)
     updateBalanceValues()
 }
 
@@ -75,13 +83,22 @@ form.addEventListener('submit', event => {
     event.preventDefault() //impedidno que o form seja enviado. Precisa garantir que tudo foi preenchido. 
     // verificação 
     const transactionName = inputTransactionName.value.trim() //para armazenar esses valores do dummyTransaction
-    const transactionsAmount = inputTransactionAmount.value.trim()
-    if (transactionName === '' || transactionsAmount === '') {
+    const transactionAmount = inputTransactionAmount.value.trim()
+    if (transactionName === '' || transactionAmount === '') {
         alert('Por favor preencha por completo a transação')
         return 
     }
     // dois inputs preenchidos 
     // objeto que representa a transação
-    const transaction =  { id: generateID(), name: transactionName, amount: transactionsAmount }
+    const transaction =  { 
+        id: generateID(), 
+        name: transactionName, 
+        amount: Number(transactionAmount)
+    }
     
+    transactions.push(transaction)
+    init()
+
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
 })
